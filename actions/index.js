ea20a4ed60a1293b09c7df52a4c2f1aa085da3df
Gregg Larson <larsongb@gmail.com>
2020-03-01 10:30:48 -0700

    clean up action

--- actions/index.js
+++ actions/index.js
@@ -1,13 +1,13 @@
 import Base64 from '../Base64'
 
 export const addBLE = (device) => ({
-    type: "ADD_BLE",
-    device
+  type: "ADD_BLE",
+  device
 })
 
 export const connectedDevice = (device) => ({
-    type: "CONNECTED_DEVICE",
-    connectedDevice: device
+  type: "CONNECTED_DEVICE",
+  connectedDevice: device
 });
 
 export const connectedServiceCharacteristics = (characteristic) => ({
@@ -31,116 +31,113 @@ export const selectedCharacteristic = (characteristic) => ({
 });
 
 export const changeStatus = (status) => ({
-    type: "CHANGE_STATUS",
-    status: status
+  type: "CHANGE_STATUS",
+  status: status
 });
 
 //some thunks to control the BLE Device
 
 export const startScan = () => {
-    return (dispatch, getState, DeviceManager) => {
-        // you can use Device Manager here
-        const subscription = DeviceManager.onStateChange((state) => {
-            if (state === 'PoweredOn') {
-                dispatch(scan());
-                subscription.remove();
-            }
-        }, true);
-      };
+  return (dispatch, getState, DeviceManager) => {
+    // you can use Device Manager here
+    const subscription = DeviceManager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+        dispatch(scan());
+        subscription.remove();
+      }
+    }, true);
+  };
 }
 
 export const scan = () => {
-    return (dispatch, getState, DeviceManager) => {
-        DeviceManager.startDeviceScan(null, null, (error, device) => {
-           dispatch(changeStatus("Scanning"));
-          if (error) {
-            console.log(error);
-          }
-          if(device !== null){
-            dispatch(addBLE(device));
-        }
-        });
-    }
+  return (dispatch, getState, DeviceManager) => {
+    DeviceManager.startDeviceScan(null, null, (error, device) => {
+      dispatch(changeStatus("Scanning"));
+      if (error) {
+        console.log(error);
+      }
+      if (device !== null) {
+        dispatch(addBLE(device));
+      }
+    });
+  }
 }
 
 export const getServiceCharacteristics = (service) => {
   return (dispatch, getState, DeviceManager) => {
     let state = getState();
-    DeviceManager.characteristicsForDevice(state.BLEs.connectedDevice.id,service.uuid)
-    .then((characteristics) => {
-      dispatch(connectedServiceCharacteristics(characteristics));
-     });
-    
+    DeviceManager.characteristicsForDevice(state.BLEs.connectedDevice.id, service.uuid)
+      .then((characteristics) => {
+        dispatch(connectedServiceCharacteristics(characteristics));
+      });
+
   }
 }
 
 export const connectDevice = (device) => {
-    return (dispatch, getState, DeviceManager) => {
-           dispatch(changeStatus("Connecting"));
-           DeviceManager.stopDeviceScan()
-           device.connect()
-              .then((device) => {
-                dispatch(changeStatus("Discovering"));
-                let allCharacteristics = device.discoverAllServicesAndCharacteristics()
-                dispatch(connectedDevice(device));
-                return allCharacteristics;
-              })
-              .then((device) => {
-                let services = device.services(device.id);
-                return services; 
-              })
-              .then((services) => {
-                console.log("found services: ", services)
-                dispatch(connectedDeviceServices(services));
-              }, (error) => {
-                console.log(this._logError("SCAN", error));
-              })
-              
-    }
+  return (dispatch, getState, DeviceManager) => {
+    dispatch(changeStatus("Connecting"));
+    DeviceManager.stopDeviceScan()
+    device.connect()
+      .then((device) => {
+        dispatch(changeStatus("Discovering"));
+        let allCharacteristics = device.discoverAllServicesAndCharacteristics()
+        dispatch(connectedDevice(device));
+        return allCharacteristics;
+      })
+      .then((device) => {
+        let services = device.services(device.id);
+        return services;
+      })
+      .then((services) => {
+        console.log("found services: ", services)
+        dispatch(connectedDeviceServices(services));
+      }, (error) => {
+        console.log(this._logError("SCAN", error));
+      })
+
+  }
 }
 
 const crcVal = (array) => {
   let currentVal;
-  let output = array.reduce(function(currentVal,index){
-      currentVal = currentVal + index;
-      if(currentVal > 256){
-        currentVal = currentVal - 256;
-      }
-      return currentVal;
+  let output = array.reduce(function (currentVal, index) {
+    currentVal = currentVal + index;
+    if (currentVal > 256) {
+      currentVal = currentVal - 256;
+    }
+    return currentVal;
   })
-  return 255-output;
+  return 255 - output;
 }
 
 function str2ab(str) {
   console.log("string to send: ", str)
   var bufView = new Uint8Array(str.length);
-  for (var i=0, strLen=str.length; i < strLen; i++) {
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
   return bufView;
 }
 
-
 export const writeCharacteristic = (text) => {
   return (dispatch, getState, DeviceManager) => {
     const state = getState();
     let buffer = str2ab(text)
-  let packetsize = 20;
-  let offset = 0;
-  let packetlength = packetsize;
-  do{
-    if(offset + packetsize > buffer.length){
-      packetlength = buffer.length;
-    } else {
-      packetlength = offset + packetsize;
-    }
-    let packet = buffer.slice(offset,packetlength);
-    console.log("packet: ", packet)
-    let base64packet = Base64.btoa(String.fromCharCode.apply(null, packet));
-    state.BLEs.connectedDevice.writeCharacteristicWithoutResponseForService(state.BLEs.selectedService.uuid,state.BLEs.selectedCharacteristic.uuid, base64packet)
-    offset += packetsize;
-  } while(offset < buffer.length)
-   // let base64color = btoa(String.fromCharCode.apply(null, buffer));
-    //state.BLEs.connectedDevice.writeCharacteristicWithResponseForService(state.BLEs.selectedService.uuid,state.BLEs.selectedCharacteristic.uuid, base64color)
+    let packetsize = 20;
+    let offset = 0;
+    let packetlength = packetsize;
+    do {
+      if (offset + packetsize > buffer.length) {
+        packetlength = buffer.length;
+      } else {
+        packetlength = offset + packetsize;
+      }
+      let packet = buffer.slice(offset, packetlength);
+      console.log("packet: ", packet)
+      let base64packet = Base64.btoa(String.fromCharCode.apply(null, packet));
+      state.BLEs.connectedDevice.writeCharacteristicWithoutResponseForService(state.BLEs.selectedService.uuid, state.BLEs.selectedCharacteristic.uuid, base64packet)
+      offset += packetsize;
+    } while (offset < buffer.length)
   }
 }
